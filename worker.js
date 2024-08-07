23d0af215d51ed567cd3464f22882a9cba581f20
Vincent Ekechukwu <136026786+vincetoby@users.noreply.github.com>
2024-08-07 20:05:44 +0100

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,35 @@
+//worker.js
+
+import Queue from 'bull';
+import dbClient from './utils/db';
+import imageThumbnail from 'image-thumbnail';
+import fs from 'fs';
+import { promisify } from 'util';
+import { join } from 'path';
+
+const fileQueue = new Queue('fileQueue');
+const writeFileAsync = promisify(fs.writeFile);
+
+fileQueue.process(async (job, done) => {
+  const { fileId, userId } = job.data;
+
+  if (!fileId) throw new Error('Missing fileId');
+  if (!userId) throw new Error('Missing userId');
+
+  const file = await dbClient.db.collection('files').findOne({
+    _id: new ObjectId(fileId),
+    userId: new ObjectId(userId),
+  });
+
+  if (!file) throw new Error('File not found');
+
+  const sizes = [500, 250, 100];
+  for (const size of sizes) {
+    const options = { width: size };
+    const thumbnail = await imageThumbnail(file.localPath, options);
+    const thumbnailPath = `${file.localPath}_${size}`;
+    await writeFileAsync(thumbnailPath, thumbnail);
+  }
+
+  done();
+});
