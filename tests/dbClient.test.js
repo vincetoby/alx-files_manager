2967f78d527cefc5de39a0b1f9eff084e71934d5
Vincent Ekechukwu <136026786+vincetoby@users.noreply.github.com>
2024-08-07 21:00:45 +0100

    Update dbClient.test.js

--- tests/dbClient.test.js
+++ tests/dbClient.test.js
@@ -1,9 +1,10 @@
+import { expect } from 'chai';
 import dbClient from '../utils/db';
 
 describe('dbClient', () => {
   it('should return a MongoDB connection', async () => {
     const collections = await dbClient.db.listCollections().toArray();
-    expect(collections).toBeInstanceOf(Array);
+    expect(collections).to.be.an('array');
   });
 
   it('should insert and retrieve a document', async () => {
@@ -12,7 +13,7 @@ describe('dbClient', () => {
     await collection.insertOne(mockDocument);
 
     const retrievedDocument = await collection.findOne({ name: 'test' });
-    expect(retrievedDocument.name).toBe('test');
+    expect(retrievedDocument.name).to.equal('test');
 
     await collection.deleteMany({});
   });
