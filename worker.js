8c9005e7bd8e93ff9e237d9480831c373f829c8f
Vincent Ekechukwu <136026786+vincetoby@users.noreply.github.com>
2024-08-07 21:18:26 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,35 +1,64 @@
 //worker.js
 
-import Queue from 'bull';
+import Bull from 'bull';
 import dbClient from './utils/db';
-import imageThumbnail from 'image-thumbnail';
-import fs from 'fs';
-import { promisify } from 'util';
-import { join } from 'path';
 
-const fileQueue = new Queue('fileQueue');
-const writeFileAsync = promisify(fs.writeFile);
+// Create a Bull queue for user-related tasks
+const userQueue = new Bull('userQueue');
 
+// Process the userQueue
+userQueue.process(async (job, done) => {
+  const { userId } = job.data;
+
+  if (!userId) {
+    return done(new Error('Missing userId'));
+  }
+
+  const user = await dbClient.db.collection('users').findOne({ _id: new ObjectID(userId) });
+
+  if (!user) {
+    return done(new Error('User not found'));
+  }
+
+  console.log(`Welcome ${user.email}!`);
+  done();
+});
+
+// Create a Bull queue for file-related tasks
+const fileQueue = new Bull('fileQueue');
+
+// Process the fileQueue
 fileQueue.process(async (job, done) => {
   const { fileId, userId } = job.data;
 
-  if (!fileId) throw new Error('Missing fileId');
-  if (!userId) throw new Error('Missing userId');
-
-  const file = await dbClient.db.collection('files').findOne({
-    _id: new ObjectId(fileId),
-    userId: new ObjectId(userId),
-  });
-
-  if (!file) throw new Error('File not found');
-
-  const sizes = [500, 250, 100];
-  for (const size of sizes) {
-    const options = { width: size };
-    const thumbnail = await imageThumbnail(file.localPath, options);
-    const thumbnailPath = `${file.localPath}_${size}`;
-    await writeFileAsync(thumbnailPath, thumbnail);
+  if (!fileId) {
+    return done(new Error('Missing fileId'));
+  }
+  if (!userId) {
+    return done(new Error('Missing userId'));
   }
 
-  done();
+  const file = await dbClient.db.collection('files').findOne({ _id: new ObjectID(fileId), userId });
+
+  if (!file) {
+    return done(new Error('File not found'));
+  }
+
+  const imageThumbnail = require('image-thumbnail');
+  const fs = require('fs').promises;
+  const path = require('path');
+
+  try {
+    const originalPath = file.localPath;
+    const sizes = [500, 250, 100];
+
+    for (const size of sizes) {
+      const thumbnail = await imageThumbnail(originalPath, { width: size });
+      const thumbnailPath = `${originalPath}_${size}`;
+      await fs.writeFile(thumbnailPath, thumbnail);
+    }
+    done();
+  } catch (error) {
+    done(error);
+  }
 });
