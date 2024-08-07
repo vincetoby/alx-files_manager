e7404cbc09c31b8f94e2b5f5fe22d1ddf3778668
Vincent Ekechukwu <136026786+vincetoby@users.noreply.github.com>
2024-08-07 14:28:30 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ import express from 'express';
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
 
